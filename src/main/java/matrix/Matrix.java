34b1274092780c2b51d8fa4adccd3d1a8ef54b1d
rummolprod999 <rummolprod999@gmail.com>
2021-11-12 16:20:32 +0300

    fix issues

--- src/main/java/matrix/Matrix.java
+++ src/main/java/matrix/Matrix.java
@@ -13,10 +13,10 @@ public class Matrix {
     }
 
     public static void shiftLeft(short[][] m, int steps) {
-        for (int i = 0; i < m.length; i++) {
-            int realSteps = steps % m[i].length;
-            while (realSteps-- > 0) {
-                short temp = m[i][0];
+        int realSteps = steps % (m[0].length * m.length);
+        while (realSteps-- > 0) {
+            short temp = m[0][0];
+            for (int i = m.length - 1; i >= 0; i--) {
                 for (int j = m[i].length - 1; j >= 0; j--) {
                     short val = m[i][j];
                     m[i][j] = temp;
